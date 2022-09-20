ddb19b8f4d77fdcd63c7b52f056eed08dce9a673
Leandro Ferreira <leanfj@hotmail.com>
2022-09-20 10:03:20 -0300

    Criado conexão com backend

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,21 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { FlatList, Image, View } from 'react-native';
 
 import logoImg from '../../assets/logo-nlw-esports.png'
-import { GameCard } from '../../components/GameCard';
+import { GameCard, GameCardProps } from '../../components/GameCard';
 import { Heading } from '../../components/Heading';
-import  {GAMES} from '../../utils/games';
-
 
 import { styles } from './styles';
 
+
 export function Home() {
+  const [games, setGames] = useState<GameCardProps[]>([]);
+
+  useEffect(() => {
+    fetch(('http://192.168.15.126:3333/games')).then(response => response.json()).then(data => setGames(data));
+  }, []);
   return (
     <View style={styles.container}>
         <Image source={logoImg} style={styles.logo} />
         <Heading title="Encontre seu Duo" subTitle="Selecione o game que deseja jogar..."/>
         <FlatList
-          data={GAMES}
+          data={games}
           keyExtractor={item => item.id}
           renderItem={({ item }) => (
             <GameCard data={item} />
